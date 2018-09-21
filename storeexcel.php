df4171b733dffc3ced3cb9d673abd10d1e91908e
Morten Sickel <morten@sickel.net>
2018-09-21 22:06:32 +0200

    added storeexcel.php

---
+++ storeexcel.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'fetchdata.php';
+require_once '../PHP_XLSXWriter/ExcelWrt.php';
+
+$rows=getobservations($table,$project);
+
+$xls = new Excel($project);
+foreach ($rows as $num => $row) {
+  $xls->home();
+  $xls->label($row['id']);
+  $xls->right();
+  $xls->label($row['title']);
+  $xls->down();
+}
+ob_start();
+$data = ob_get_clean();
+file_put_contents(__DIR__ .'/report.xls', $data);
