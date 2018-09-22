45e4740078b6abb65beac00daa831f756cfdeef0
Morten Sickel <morten@sickel.net>
2018-09-22 20:16:21 +0200

    Some layout fixes

--- show.php
+++ show.php
@@ -1,24 +1,26 @@
 <?php
 
 require_once 'fetchdata.php';
-
+n=count($data);
 ?>
-
 <form>
 Project: <input type="text" name="project" value="<?php echo($project)?>"/>
 <input type="submit" value="View"/>
 </form>
-
-
+<hr />
 <?php
+
+print "<p><a href=\"storeexcel.php?project=$project\">Save excel</a>";
+print " <a href=\"storekml.php?project=$project\">Save kml</a></p>";
+print "<p>$n datapoints</p>";
+
+
 $data=useroverview($table,$project);
 htmlusertable($data);
 
 $data=getobservations($table,$project);
 htmltablehead($tablehead);
 htmlobsdata($data);
-print "<p><a href=\"storeexcel.php?project=$project\">Save excel</a></p>";
-print "<p><a href=\"storekml.php?project=$project\">Save kml</a></p>";
 
 ?>
 </body>
